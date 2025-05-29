59067dd200eda9020d1422cb45bf5348127f997a
VladimirZadorozhny <zadorozhny.vladimir@gmail.com>
2025-05-29 17:37:31 +0200

    javadoc plugin toegevoegd.

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -2,18 +2,41 @@ package be.vdab.goededoel;
 
 import java.math.BigDecimal;
 
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author John Dalton
+ */
 public class GoedeDoel {
+
 	private final String naam;
 	private BigDecimal opbrengst = BigDecimal.ZERO;
+
+	/**
+	 * Maakt een GoedeDoel object
+	 * @param naam De naam van het goede doel
+	 */
 
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
 
+	/**
+	 * Geeft de naam terug
+	 * @return De naam van het goede doel
+	 */
 	public String getNaam() {
 		return naam;
 	}
 
+
+	public void setOpbrengst(BigDecimal opbrengst) {
+		this.opbrengst = opbrengst;
+	}
+
+	/**
+	 * Geeft de opbrengst terug
+	 * @return De opbrengst van het goede doel
+	 */
 	public BigDecimal getOpbrengst() {
 		return opbrengst;
 	}
